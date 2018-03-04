be7553c7a87bf658b6cec579f679db8292a02aaf
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-04 00:19:02 +0000

    add Account as tenant model

---
+++ spec/views/accounts/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'accounts/index', type: :view do
+  before(:each) do
+    assign(:accounts, [
+             Account.create!(
+               name: 'Name',
+               website: 'Website'
+             ),
+             Account.create!(
+               name: 'Name',
+               website: 'Website'
+             )
+           ])
+  end
+
+  it 'renders a list of accounts' do
+    render
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
+    assert_select 'tr>td', text: 'Website'.to_s, count: 2
+  end
+end
