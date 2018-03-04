c6cedee094d0d6a1bbca078e090d08bfea1b554b
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-04 00:19:02 +0000

    add Account as tenant model

---
+++ spec/views/accounts/new.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'accounts/new', type: :view do
+  before(:each) do
+    assign(:account, Account.new(
+                       name: 'MyString',
+                       website: 'MyString'
+    ))
+  end
+
+  it 'renders new account form' do
+    render
+
+    assert_select 'form[action=?][method=?]', accounts_path, 'post' do
+      assert_select 'input[name=?]', 'account[name]'
+
+      assert_select 'input[name=?]', 'account[website]'
+    end
+  end
+end
