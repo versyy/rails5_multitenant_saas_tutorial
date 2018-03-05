9242ad33e70ef153954524b968ae0c470c4bcd62
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-05 19:05:35 +0000

    configure environment/application_controller for devise

---
+++ spec/support/devise.rb
@@ -0,0 +1,7 @@
+require_relative './request_helper'
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include RequestHelper, type: :request
+end
