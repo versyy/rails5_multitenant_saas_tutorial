2044276bc9c3e4e383be831b3c3e52fa4ae66b8d
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-12 22:18:47 +0000

    add basic role model specs and factories

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    expect(build(:role)).to be_valid
+  end
+
+  it 'is valid with role_owner trait' do
+    expect(build(:role_owner)).to be_valid
+  end
+
+  it 'is valid with role_member trait' do
+    expect(build(:role_member)).to be_valid
+  end
 end
