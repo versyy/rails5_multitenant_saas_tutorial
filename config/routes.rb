f16789d57cc62c1143fd91482606165b021d5fe4
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-12 01:18:41 +0000

    add routing and security overrides to accept invitations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
+    invitations: 'users/invitations',
     passwords: 'users/passwords',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
