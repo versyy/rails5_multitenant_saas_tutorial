9232e7ede74952278e598b6f9b626d5eb7e4be2b
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-13 02:27:49 +0000

    add basic role assignments to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
   rolify
   acts_as_tenant(:account)
+  after_create :assign_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def assign_default_role
+    if account.users.count == 1 && roles.blank?
+      add_role(:owner)
+    else
+      add_role(:member)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:first_user) { create(:user) }
+
   it 'is valid with valid attributes' do
     expect(build(:user)).to be_valid
   end
 
   it 'is invalid without password' do
     expect(build(:user, password: nil)).to be_invalid
+  end
+
+  it 'is assigned the :owner role when it is the first user in an account' do
+    expect(first_user.account.users.count).to eq(1)
+    expect(first_user.is_owner?).to be
+  end
+
+  it 'is assigned the :member role when it is not the first user in an acccount' do
+    second_user = create(:user, account: first_user.account)
+    expect(second_user.is_owner?).not_to be
+    expect(second_user.is_member?).to be
   end
 end
