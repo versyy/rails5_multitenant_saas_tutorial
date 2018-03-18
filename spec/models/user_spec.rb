c3ec53251ec2263b083335c2f988ea9b45e85f6a
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-18 04:01:13 +0000

    add safe_attributes to user model for use with 3rd party tools

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,16 @@ RSpec.describe User, type: :model do
       expect(user.roles.empty?).to be
     end
   end
+
+  context '#safe_attributes' do
+    subject { user.safe_attributes }
+
+    it { is_expected.to be_a(Hash) }
+    it { is_expected.to include(first_name: user.first_name) }
+    it { is_expected.to include(last_name: user.last_name) }
+    it { is_expected.to include(email: user.email) }
+    it { is_expected.to include(last_sign_in_at: user.last_sign_in_at) }
+    it { is_expected.to include(updated_at: user.updated_at) }
+    it { is_expected.to include(created_at: user.created_at) }
+  end
 end
