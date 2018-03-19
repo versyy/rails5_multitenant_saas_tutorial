175747b0bcbd890b924cedf0dd045c53bf6c9492
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-19 05:18:35 +0000

    configure sidekiq environment

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,12 @@
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV.fetch('REDIS_URL'),
+    network_timeout: 5
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV.fetch('REDIS_URL')
+  }
+end
