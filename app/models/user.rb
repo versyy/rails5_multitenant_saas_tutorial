0a033549946c18608804c062d4d548e4278e53af
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-19 05:21:09 +0000

    override devise messages to use activejob delayed delivery

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,10 @@ class User < ApplicationRecord
     }
   end
 
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
   private
 
   def assign_default_role
