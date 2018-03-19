6e0857c75f8d883fabe00be9a4908fd9cecac398
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-03-19 05:20:07 +0000

    connect activejob to sidekiq and define queues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,10 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails5_multitenant_saas_tutorial_#{Rails.env}"
+  config.active_job.queue_adapter     = :sidekiq
+  config.active_job.queue_name_prefix = 'rmst_activejob'
+
+  # Set mailer caching behavior
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: ENV.fetch('HOST_NAME') }
