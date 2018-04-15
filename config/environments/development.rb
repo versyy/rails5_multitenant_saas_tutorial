c1a6ea694b5853e67cf994ff4f8fdf0cff7b3052
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-04-15 20:13:53 +0000

    add ip whitelisting, for development environment tessting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Add whitelisting
+  config.web_console.whitelisted_ips = '192.168.0.0/16'
 end
