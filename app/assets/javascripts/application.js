e2ef45a16e4db7d89feffd1ce5fd156754e16a55
Andrew Conrad <andrewconrad@sloan.mit.edu>
2018-04-17 04:46:05 +0000

    add asset configuration for bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
-//= require_tree .
+//= require popper
+//= require bootstrap
